89eab54b28e511faf92c4aed9fa17308e17229bb
mudrii <mudreac@gmail.com>
2023-12-25 16:36:09 +0800

    added additional plugins and configuration to init.lua neovim

--- modules/home-manager/dotfiles/nvim.nix
+++ modules/home-manager/dotfiles/nvim.nix
@@ -4,61 +4,61 @@
     vimAlias = true;
     defaultEditor = true;
     plugins = with pkgs.vimPlugins; [
-      telescope-nvim
-      vim-airline
-      vim-airline-themes
-      harpoon2
-      undotree
-      # tmux-nvim
-      # copilot-lua
-      copilot-vim
-      rose-pine
-      lsp-colors-nvim
-      lsp-zero-nvim
-      nvim-lspconfig
-      mason-lspconfig-nvim
-      mason-nvim
-      nvim-cmp
-      cmp-buffer
-      cmp-path
-      cmp_luasnip
-      cmp-nvim-lsp
-      cmp-nvim-lua
-      luasnip
-      friendly-snippets
-      vim-fugitive
-      nvim-treesitter
-      nvim-treesitter-parsers.dockerfile
-      nvim-treesitter-parsers.c
-      nvim-treesitter-parsers.lua
-      nvim-treesitter-parsers.rust
-      nvim-treesitter-parsers.fish
-      nvim-treesitter-parsers.go
-      nvim-treesitter-parsers.json
-      nvim-treesitter-parsers.nix
-      nvim-treesitter-parsers.markdown
-      nvim-treesitter-parsers.python
-      nvim-treesitter-parsers.sql
-      nvim-treesitter-parsers.terraform
-      nvim-treesitter-parsers.javascript
-      nvim-treesitter-parsers.typescript
-      nvim-treesitter-parsers.yaml
-      nvim-treesitter-parsers.zig
-      nvim-treesitter-parsers.ocaml
+      telescope-nvim # Telescope is a highly extendable fuzzy finder over lists.
+      comment-nvim # Comment out lines of code based on the file type.
+      nvim-ts-context-commentstring # Set the commentstring based on the cursor location in the file.
+      nvim-web-devicons # Show file icons in Nvim.
+      gruvbox # A retro groove color scheme for Vim.
+      tokyonight-nvim # A dark color scheme for Neovim.
+      onedark-nvim # A dark color scheme for Neovim.
+      lualine-nvim # A light and configurable statusline/tabline plugin for Neovim written in Lua.
+      harpoon2 # A navigation utility plugin for Neovim.
+      undotree # A plugin for visualizing undo history.
+      # tmux-nvim # A plugin for controlling Tmux from Nvim.
+      # copilot-lua # A plugin for generating code with GitHub Copilot.
+      copilot-vim # A plugin for generating code with GitHub Copilot.
+      rose-pine # A color scheme for Neovim.
+      lsp-colors-nvim # A plugin for highlighting LSP diagnostics.
+      lsp-zero-nvim # A plugin for configuring LSP in Neovim.
+      nvim-lspconfig # A collection of configurations for Neovim's built-in LSP client.
+      mason-lspconfig-nvim # A plugin for configuring LSP in Neovim.
+      mason-nvim # A plugin for configuring Neovim.
+      nvim-cmp # A completion plugin for Neovim.  
+      cmp-buffer # A buffer source for nvim-cmp.
+      cmp-path # A path source for nvim-cmp.
+      cmp_luasnip # A LuaSnip source for nvim-cmp.
+      cmp-nvim-lsp # A LSP source for nvim-cmp.
+      cmp-nvim-lua # A Lua source for nvim-cmp.
+      luasnip # A snippet engine for Neovim.
+      friendly-snippets # A collection of snippets for LuaSnip.
+      vim-fugitive # A Git wrapper for Neovim.
+      nvim-treesitter # A parser generator tool and an incremental parsing library.
+      nvim-treesitter-parsers.dockerfile # A parser for Dockerfiles.
+      nvim-treesitter-parsers.c # A parser for C.
+      nvim-treesitter-parsers.lua # A parser for Lua.
+      nvim-treesitter-parsers.rust # A parser for Rust.
+      nvim-treesitter-parsers.fish # A parser for Fish.
+      nvim-treesitter-parsers.go # A parser for Go.
+      nvim-treesitter-parsers.json # A parser for JSON.
+      nvim-treesitter-parsers.nix # A parser for Nix.
+      nvim-treesitter-parsers.markdown # A parser for Markdown.
+      nvim-treesitter-parsers.python # A parser for Python.
+      nvim-treesitter-parsers.sql # A parser for SQL.
+      nvim-treesitter-parsers.terraform # A parser for Terraform.
+      nvim-treesitter-parsers.javascript # A parser for JavaScript.
+      nvim-treesitter-parsers.typescript # A parser for TypeScript.
+      nvim-treesitter-parsers.yaml # A parser for YAML.
+      nvim-treesitter-parsers.zig # A parser for Zig.
+      nvim-treesitter-parsers.ocaml # A parser for OCaml.
+      nvim-treesitter-parsers.java # A parser for Java.
     ];
 
     extraLuaConfig = ''
       vim.g.mapleader = " "  -- Set the global leader key to the space bar. The leader key is used in combination with other keys for custom key mappings.
       vim.g.maplocalleader = ' '  -- Set the local leader key to the space bar. The local leader can be used for buffer-local key mappings.
 
-      -- Airline configuration
-      vim.g.airline_powerline_fonts = 1 -- Use Powerline fonts for Airline.
-
-      vim.g['airline#extensions#line#enabled'] = 1 -- Enable the line extension.
-      vim.g['airline#extensions#tabline#show_buffers'] = 0 -- Disable the tabline extension.
-      vim.g.airline_theme = 'dark' -- Set the Airline theme to dark.
-      vim.g['airline#extensions#hunks#enabled'] = 0 -- Disable the hunks extension.
-      vim.g['airline#extensions#branch#enabled'] = 1 -- Enable the branch extension.
+      -- Lualine
+      require('lualine').setup()  -- Initialize Lualine with default settings.
 
       -- Colorscheme configuration
       require('rose-pine').setup({
@@ -90,10 +90,6 @@
       cmp_mappings['<Tab>'] = nil
       cmp_mappings['<S-Tab>'] = nil
 
-      -- lsp.setup_nvim_cmp({
-      --  mapping = cmp_mappings
-      -- })
-
       lsp.set_preferences({
           suggest_lsp_servers = false,
           sign_icons = {
@@ -103,6 +99,40 @@
               info = 'I'
           }
       })
+      
+      -- Comment configuration
+      local status_ok, comment = pcall(require, "Comment")
+      if not status_ok then
+        return
+      end
+
+      comment.setup {
+        pre_hook = function(ctx)
+          local U = require "Comment.utils"
+
+          local status_utils_ok, utils = pcall(require, "ts_context_commentstring.utils")
+          if not status_utils_ok then
+            return
+          end
+
+          local location = nil
+          if ctx.ctype == U.ctype.block then
+            location = utils.get_cursor_location()
+          elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+            location = utils.get_visual_start_location()
+          end
+
+          local status_internals_ok, internals = pcall(require, "ts_context_commentstring.internals")
+          if not status_internals_ok then
+            return
+          end
+
+          return internals.calculate_commentstring {
+            key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
+            location = location,
+          }
+        end,
+      }
 
       -- Telescope configuration      
       local builtin = require('telescope.builtin')  -- Load the Telescope built-in functions.

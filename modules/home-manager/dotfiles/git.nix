a907b7b1a0654df7f3d2e3ca571152b654cb5c3a
mudrii <mudreac@gmail.com>
2023-12-27 22:17:03 +0800

    added additional gitignore optiopns for .DS_Store

--- modules/home-manager/dotfiles/git.nix
+++ modules/home-manager/dotfiles/git.nix
@@ -6,7 +6,7 @@
     lfs.enable = true;
     userName = "mudrii";
     userEmail = "mudreac@gmail.com";
-    ignores = [ "*~" "*.swp" ".DS_Store" ];
+    ignores = [ "*~" "*.swp" ".DS_Store" "._.DS_Store" "**/.DS_Store" "**/._.DS_Store" ];
 /*
     signing = {
       key = "C37CEF50333B225E2FCA7D2003B8C6E70C3ED787";

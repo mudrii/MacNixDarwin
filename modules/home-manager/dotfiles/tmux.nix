e4f9a901f4da711e19c53c6ff48890f8095e8834
mudrii <mudreac@gmail.com>
2024-06-24 09:56:38 +0400

    tmux cleanup

--- modules/home-manager/dotfiles/tmux.nix
+++ modules/home-manager/dotfiles/tmux.nix
@@ -76,7 +76,7 @@
         bind C-f command-prompt -p find-session 'switch-client -t %%'
         
         # Ctrl+a Ctrl+l, tmux will send the Ctrl+L key sequence to the terminal, clear the screen, and repaint the command line.
-        bind-key -n C-l send-keys C-l \; send-keys "clear && commandline -f repaint"
+        bind-key -n C-l send-keys C-l 
 
         # Vim style Movement commands
         bind h select-pane -L
